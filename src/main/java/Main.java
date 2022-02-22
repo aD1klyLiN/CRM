6a9982b4d6e3bdaa6ddebecede8ab728bcf40e2f
aD1KlyLiN <lylin@bk.ru>
2022-02-22 15:24:10 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.ArrayList;
 
 public class Main extends JFrame {
 
@@ -9,7 +14,7 @@ public class Main extends JFrame {
     private JButton button3;
     private JButton button4;
     private JButton button5;
-    private JList list1;
+    private JList<String> list1;
     private JCheckBox checkBox1;
     private JCheckBox checkBox2;
     private JCheckBox checkBox3;
@@ -19,26 +24,64 @@ public class Main extends JFrame {
     private JCheckBox checkBox7;
     private JCheckBox checkBox8;
 
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final String WINDOW_TITLE = "CRM Main Window";
+
+    private ArrayList<String> list;
+    private DefaultListModel<String> dlm;
+
     public Main() throws HeadlessException {
-        super();
+        super(WINDOW_TITLE);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
         Rectangle screenBounds = getGraphicsConfiguration().getBounds();
-        int width = 640;
-        int height = 480;
-        int x = screenBounds.width/2 - width/2;
-        int y = screenBounds.height/2 - height/2;
-        setBounds(x, y, width, height);
+        int x = screenBounds.width/2 - WIDTH/2;
+        int y = screenBounds.height/2 - HEIGHT/2;
+        setBounds(x, y, WIDTH, HEIGHT);
+
+        checkBox1.addItemListener(new ActionL());
+        checkBox2.addItemListener(new ActionL());
+        checkBox3.addItemListener(new ActionL());
+        checkBox4.addItemListener(new ActionL());
+        checkBox5.addItemListener(new ActionL());
+        checkBox6.addItemListener(new ActionL());
+        checkBox7.addItemListener(new ActionL());
+        checkBox8.addItemListener(new ActionL());
+
+        dlm = new DefaultListModel<>();
+        list = new ArrayList<>();
+        dlm.addAll(list);
+        list1.setModel(dlm);
+
         setContentPane(rootPanel);
         setResizable(false);
         setVisible(true);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                Main main = new Main();
+        SwingUtilities.invokeLater(Main::new);
+    }
+
+    private class ActionL implements ActionListener, ItemListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+
+            System.out.println(actionEvent.getActionCommand());
+
+        }
+
+        @Override
+        public void itemStateChanged(ItemEvent itemEvent) {
+            System.out.println(itemEvent.getStateChange() == ItemEvent.SELECTED);
+            if (itemEvent.getStateChange() == ItemEvent.SELECTED) {
+                dlm.addElement(itemEvent.paramString());
+            } else {
+                dlm.removeElement(itemEvent.paramString());
             }
-        });
+            System.out.println(itemEvent.paramString());
+
+        }
     }
 }
